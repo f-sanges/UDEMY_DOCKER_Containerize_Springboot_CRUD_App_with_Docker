fefbbfe8c50d35339ae069d657efe0e2c9e4a3cd
fsanges <FSanges.Capgemini@esterno.agosducato.it>
2022-01-11 18:46:12 +0100

    Exercise modified from the course

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:11
+EXPOSE 8080
+ADD demo-0.0.1-SNAPSHOT.jar demo.jar
+ENTRYPOINT ["java", "-jar", "demo.jar"]
---
+++ demo/demo/src/main/java/com/example/demo/controller/GreetingsController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class GreetingsController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to the World of Containers";
+
+    }
+}
